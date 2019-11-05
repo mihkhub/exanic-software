659e274bd2a08fc5cae37e8d649fe30e65ac6fd3
patqs <ccball2010@163.com>
2019-11-05 23:40:31 +0800

    Round-Robin UDP send

---
+++ examples/exasock/udp-rr-send.c
@@ -0,0 +1,105 @@
+#include <poll.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in send_addr, rr_addr;
+    int send_fd, port, cur_fd_index;
+    char *p, buf[1024], pktbuf[2048], dev[16];
+    ssize_t len;
+    int *fds;
+    int num_fds, num_packets, samples;
+    int num_fds_mask;
+    unsigned int seq = 1;
+
+    /* Parse command line arguments */
+
+    if (argc != 5)
+        goto usage_error;
+
+    send_addr.sin_family = AF_INET;
+    if (inet_aton(argv[1], &send_addr.sin_addr) == 0)
+        goto usage_error;
+    send_addr.sin_port = htons(strtol(argv[2], &p, 10));
+    if (*argv[2] == '\0' || *p != '\0')
+        goto usage_error;
+
+    rr_addr.sin_family = AF_INET;
+    rr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    num_fds = atoi(argv[3]);
+
+    if ((num_fds != 1) && (num_fds & (num_fds - 1)))
+    {
+        printf("max buffers must be a power of 2\n");
+        goto usage_error;
+    }
+
+    num_fds_mask = num_fds-1;
+
+    num_packets = atoi(argv[4]);
+
+    fds = malloc(sizeof(int*)*num_fds);
+
+    for(int i = 0; i < num_fds;++i) {
+        send_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    	if (send_fd == -1)
+    	{
+    		fprintf(stderr, "socket: %s\n", strerror(errno));
+    		return EXIT_FAILURE;
+    	}
+
+    	rr_addr.sin_port = htons(7532 + i);
+	    if (bind(send_fd, (struct sockaddr *)&rr_addr, sizeof(rr_addr)) == -1)
+	    {
+	        fprintf(stderr, "bind: %s\n", strerror(errno));
+	        return EXIT_FAILURE;
+	    }
+
+        if (connect(send_fd, (struct sockaddr *)&send_addr, sizeof(send_addr)) == -1)
+        {
+            fprintf(stderr, "connect: %s\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
+        fds[i] = send_fd;
+    }
+
+
+    /* Send data on connection */
+    cur_fd_index = 0;
+    samples = 0;
+    do {
+		len = sizeof(seq);
+
+		if (send(fds[cur_fd_index], &seq, len, 0) == -1)
+		{
+			fprintf(stderr, "send: %s, fd=%d\n", strerror(errno), fds[cur_fd_index]);
+			return EXIT_FAILURE;
+		}
+		++cur_fd_index;
+		cur_fd_index &= num_fds_mask;
+		++seq;
+		++samples;
+    } while( samples < num_packets);
+
+    for(int i = 0; i < num_fds;++i) {
+        close(fds[i]);
+    }
+
+    return 0;
+
+usage_error:
+    fprintf(stderr, "Usage: %s <addr> <port> num_fds num_packets\n", argv[0]);
+    printf("    num_fds: maximum number of fds to use (must be power of 2)\n");
+	printf("    num_packets: total number of packets to transmit\n");
+    return EXIT_FAILURE;
+}
