60e5a6a199a89cd19819509872e79c0c17e5fade
patqs <ccball2010@163.com>
2019-11-06 07:25:08 +0800

    UDP send per tick

--- examples/exasock/udp-rr-send.c
+++ examples/exasock/udp-rr-send.c
@@ -8,17 +8,22 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "util.h"
+
+#define TICK_NS (500 * 1000 * 1000)
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in send_addr, rr_addr;
-    int send_fd, port, cur_fd_index;
-    char *p, buf[1024], pktbuf[2048], dev[16];
+    int send_fd, cur_fd_index;
+    char *p, buf[64];
     ssize_t len;
     int *fds;
-    int num_fds, num_packets, samples;
-    int num_fds_mask;
+    int num_ports, num_packets, samples;
+    int num_ports_mask;
     unsigned int seq = 1;
+    timing_t cur_time, deadline_tick, deadline_delta_tick;
+    float cpu_ghz;
 
     /* Parse command line arguments */
 
@@ -36,21 +41,24 @@ int main(int argc, char *argv[])
     rr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 
-    num_fds = atoi(argv[3]);
+    num_ports = atoi(argv[3]);
 
-    if ((num_fds != 1) && (num_fds & (num_fds - 1)))
+    if ((num_ports != 1) && (num_ports & (num_ports - 1)))
     {
         printf("max buffers must be a power of 2\n");
         goto usage_error;
     }
 
-    num_fds_mask = num_fds-1;
+    num_ports_mask = num_ports-1;
 
     num_packets = atoi(argv[4]);
 
-    fds = malloc(sizeof(int*)*num_fds);
+    fds = malloc(sizeof(int*)*num_ports);
 
-    for(int i = 0; i < num_fds;++i) {
+    cpu_ghz = get_cpu_ghz();
+	deadline_delta_tick = to_tsc(cpu_ghz, TICK_NS);
+
+    for(int i = 0; i < num_ports;++i) {
         send_fd = socket(AF_INET, SOCK_DGRAM, 0);
     	if (send_fd == -1)
     	{
@@ -78,28 +86,40 @@ int main(int argc, char *argv[])
     cur_fd_index = 0;
     samples = 0;
     do {
-		len = sizeof(seq);
 
-		if (send(fds[cur_fd_index], &seq, len, 0) == -1)
-		{
-			fprintf(stderr, "send: %s, fd=%d\n", strerror(errno), fds[cur_fd_index]);
-			return EXIT_FAILURE;
-		}
-		++cur_fd_index;
-		cur_fd_index &= num_fds_mask;
-		++seq;
-		++samples;
+    	timing_start(cur_time);
+    	deadline_tick = cur_time + deadline_delta_tick;
+    	for(int i = 0; i < num_ports; ++i) {
+    		memcpy(buf, &seq, sizeof(seq));
+    		len = sizeof(seq);
+    		if (send(fds[cur_fd_index], buf, len, 0) == -1)
+    		{
+    			fprintf(stderr, "send: %s, fd=%d\n", strerror(errno), fds[cur_fd_index]);
+    			return EXIT_FAILURE;
+    		}
+    		++cur_fd_index;
+    		cur_fd_index &= num_ports_mask;
+    		++seq;
+    		++samples;
+    	}
+
+		do {
+			timing_start(cur_time);
+		} while ( cur_time < deadline_tick);
+
     } while( samples < num_packets);
 
-    for(int i = 0; i < num_fds;++i) {
+    for(int i = 0; i < num_ports;++i) {
         close(fds[i]);
     }
+
+    free(fds);
 
     return 0;
 
 usage_error:
-    fprintf(stderr, "Usage: %s <addr> <port> num_fds num_packets\n", argv[0]);
-    printf("    num_fds: maximum number of fds to use (must be power of 2)\n");
+    fprintf(stderr, "Usage: %s <addr> <port> num_ports num_packets\n", argv[0]);
+    printf("    num_ports: maximum number of source ports to use (must be power of 2)\n");
 	printf("    num_packets: total number of packets to transmit\n");
     return EXIT_FAILURE;
 }
